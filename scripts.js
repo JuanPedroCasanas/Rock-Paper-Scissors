f2891dc607902a41f827c3e9cc2a66893e0c4136
juan <juan.pedro.casanas99@gmail.com>
2021-08-17 11:04:44 -0300

    changed all inner.html to text.content for security reasons (Even though there is no user input)
    A lot of lines extend beyond the eightieth column, has to be fixed for clean code reasons

--- scripts.js
+++ scripts.js
@@ -24,9 +24,9 @@ play.onclick = playGame;
 reset.onclick = ()=>{
   randomElement = randomizeElement();
   computerElement = randomElement;
-  result.innerHTML = "Computer element has been changed";
-  playerScore.innerHTML = 0;
-  computerScore.innerHTML = 0;
+  result.textContent = "Computer element has been changed";
+  playerScore.textContent = 0;
+  computerScore.textContent = 0;
 }
 
 function randomizeElement (){
@@ -39,48 +39,48 @@ function randomizeElement (){
 function playRound (){
     if (playerElement===undefined || computerElement === undefined){
   } else if ((playerElement===computerElement)){
-    result.innerHTML = "Draw, you picked " + playerElement + ". Computer picked " + computerElement;
+    result.textContent = "Draw, you picked " + playerElement + ". Computer picked " + computerElement;
     randomElement = randomizeElement();
     computerElement = randomElement;
   } else if ((playerElement==="rock" && computerElement==="scissors")|| 
   (playerElement==="paper" && computerElement==="rock")|| 
   (playerElement==="scissors" && computerElement==="paper")){
-    result.innerHTML = "You win, you picked " + playerElement + ". Computer picked " + computerElement;
-    playerScore.innerHTML = ++playerScore.innerHTML;
+    result.textContent = "You win, you picked " + playerElement + ". Computer picked " + computerElement;
+    playerScore.textContent = ++playerScore.textContent;
     randomElement = randomizeElement();
     computerElement = randomElement;
   }else {
-    result.innerHTML = "You lose, you picked " + playerElement + ". Computer picked " + computerElement;
+    result.textContent = "You lose, you picked " + playerElement + ". Computer picked " + computerElement;
     randomElement = randomizeElement();
     computerElement = randomElement;
-    computerScore.innerHTML = ++computerScore.innerHTML;
+    computerScore.textContent = ++computerScore.textContent;
   }
 }
 
 function playGame(){
   if (playerElement===undefined || computerElement === undefined){
-  }else if (computerScore.innerHTML == 5 && playerScore.innerHTML == 5){
+  }else if (computerScore.textContent == 5 && playerScore.textContent == 5){
     randomElement = randomizeElement();
     computerElement = randomElement;
-    result.innerHTML = "Draw, computer element reseted";
-    playerScore.innerHTML = 0;
-    computerScore.innerHTML = 0;
-  }else if((playerScore.innerHTML == 5) && ((playerElement==="rock" && computerElement==="scissors")|| 
+    result.textContent = "Draw, computer element reseted";
+    playerScore.textContent = 0;
+    computerScore.textContent = 0;
+  }else if((playerScore.textContent == 5) && ((playerElement==="rock" && computerElement==="scissors")|| 
     (playerElement==="paper" && computerElement==="rock")|| 
     (playerElement==="scissors" && computerElement==="paper"))) {
       randomElement = randomizeElement();
       computerElement = randomElement;
-      result.innerHTML = "You won, computer element reseted";
-      playerScore.innerHTML = 0;
-      computerScore.innerHTML = 0;
-  }else if((computerScore.innerHTML == 5) && ((playerElement==="scissors" && computerElement==="rock")|| 
+      result.textContent = "You won, computer element reseted";
+      playerScore.textContent = 0;
+      computerScore.textContent = 0;
+  }else if((computerScore.textContent == 5) && ((playerElement==="scissors" && computerElement==="rock")|| 
   (playerElement==="rock" && computerElement==="paper")|| 
   (playerElement==="paper" && computerElement==="scissors"))) {   
   randomElement = randomizeElement();
   computerElement = randomElement;
-  result.innerHTML = "You lost, computer element reseted";
-  playerScore.innerHTML = 0;
-  computerScore.innerHTML = 0;
+  result.textContent = "You lost, computer element reseted";
+  playerScore.textContent = 0;
+  computerScore.textContent = 0;
   }else{
     playRound()
   }
